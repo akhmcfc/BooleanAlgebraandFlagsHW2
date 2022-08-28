e304d7107e61535e850cb26e57b3bc43f42db897
Hooda Akshat <hooda.akshat@syk.fi>
2022-08-28 12:58:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Boolean shining=true;
+        int time=15;
+        if(time>=10&&time<=16&&shining==true)
+        {
+            System.out.println("Please use Sunscreen.");
+        }
+    }
+}
